e0019b659577d32d3e94530bc78207f95920f7d6
dheerajmekala <dheeraj7596.dm@gmail.com>
2023-06-07 00:10:39 -0700

    added chatgpt yaml

--- rl4lms/testing/baselines/chatgpt.py
+++ rl4lms/testing/baselines/chatgpt.py
@@ -1,9 +1,11 @@
 import json
-import sys
+from argparse import ArgumentParser
+
 import openai
 import os
 
 import pandas as pd
+import yaml
 from ratelimiter import RateLimiter
 from rl4lms.envs.text_generation.registry import MetricRegistry
 from rl4lms.envs.text_generation.training_utils import build_datapool
@@ -30,8 +32,7 @@ def call_chatgpt(input, num_tokens):
     return cont_gpt3
 
 
-def generate_text(tokenizer, samples, max_prompt_length, num_tokens=48):
-    prompt = "Continue this text into a positive movie review.\n"
+def generate_text(prompt, tokenizer, samples, max_prompt_length, num_tokens):
     inputs = [sample.prompt_or_input_text for sample in samples]
     encodings = tokenizer(
         inputs,
@@ -73,39 +74,48 @@ def compute_and_dump_metrics(metrics, all_prompt_texts, all_generated_texts, all
 
 
 if __name__ == "__main__":
-    out_path = sys.argv[1]
-    os.makedirs(out_path, exist_ok=True)
-    data_config = {
-        "id": "imdb",
-        "args": {"seed": 42, "prompt_prefix": "", "prompt_suffix": ""}
-    }
-    batch_size = 1
-    max_prompt_length = 64
-    metric_configs = [
-        {"id": "learned_reward", "args": {"model_name": "lvwerra/distilbert-imdb", "label_ix": 1, "batch_size": 64}},
-        {"id": "diversity", "args": {}},
-    ]
+    parser = ArgumentParser(description="Run ChatGPT for tasks")
+    parser.add_argument("--config_path", type=str, help="path to the config file")
+    parser.add_argument(
+        "--out_path", type=str, help="Directory to save results"
+    )
+    args = parser.parse_args()
+
+    with open(args.config_path, "r") as fp:
+        config = yaml.safe_load(fp)
+
+    os.makedirs(args.out_path, exist_ok=True)
+    data_config = config["datapool"]
+    metric_configs = config["metrics"]
 
     tokenizer = AutoTokenizer.from_pretrained("gpt2")
     tokenizer.truncation_side = "left"
     tokenizer.padding_side = "left"
     tokenizer.pad_token = tokenizer.eos_token
+    max_prompt_length = config.get("max_prompt_length", tokenizer.model_max_length)
+    num_tokens = config["num_tokens"]
     metrics = [MetricRegistry.get(metric_config["id"], metric_config.get("args", {}))
                for metric_config in metric_configs]
     samples_by_split = build_datapool(data_config)
-    samples = samples_by_split["test"]
+    samples = samples_by_split[config.get("split", "test")]
 
     all_generated_texts = []
     all_ref_texts = []
     all_prompt_texts = []
-    for batch in tqdm(list(get_batch(samples, batch_size)), desc="Evaluating"):
-        batch_generated_texts = generate_text(tokenizer, batch, max_prompt_length)
+    for batch in tqdm(list(get_batch(samples, 1)), desc="Evaluating"):
+        batch_generated_texts = generate_text(
+            config.get("prefix", ""),
+            tokenizer,
+            batch,
+            max_prompt_length,
+            num_tokens
+        )
         batch_ref_texts = [sample.references for sample in batch]
         batch_prompt_texts = [sample.prompt_or_input_text for sample in batch]
         all_generated_texts.extend(batch_generated_texts)
         all_ref_texts.extend(batch_ref_texts)
         all_prompt_texts.extend(batch_prompt_texts)
 
-    compute_and_dump_metrics(metrics, all_prompt_texts, all_generated_texts, all_ref_texts, out_path)
+    compute_and_dump_metrics(metrics, all_prompt_texts, all_generated_texts, all_ref_texts, args.out_path)
     dic_df = pd.DataFrame.from_dict({"prompt": all_prompt_texts, "gen": all_generated_texts, "ref": all_ref_texts})
-    dic_df.to_csv(os.path.join(out_path, "out.csv"), index=False)
+    dic_df.to_csv(os.path.join(args.out_path, "out.csv"), index=False)
